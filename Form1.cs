2a1e6d0d8c4ee02aaa66afa3a321ef48195ec25a
Fannandya Sutan <sfannandya@gmail.com>
2026-03-14 01:02:42 +0700

    menambahkan konfigurasi button hitung mhs

--- Form1.cs
+++ Form1.cs
@@ -38,5 +38,28 @@ namespace PraktikumADO
             }
         }
 
+        private void btnHitungMhs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+
+                string query = "SELECT COUNT(*) FROM Mahasiswa";
+
+                cmd = new SqlCommand(query, conn);
+
+                int count = (int)cmd.ExecuteScalar();
+
+                txtHasil.Text = jumlah.ToString();
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
     }
 }
